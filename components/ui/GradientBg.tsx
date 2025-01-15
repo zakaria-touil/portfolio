cac563a7f114412a060a074d37c04fbfc5e8d75e
zakaria-touil <zakaria.touil.nouar@gmail.com>
2025-01-15 20:24:39 +0100

    add condition typeof document !== "undefined"

--- components/ui/GradientBg.tsx
+++ components/ui/GradientBg.tsx
@@ -40,15 +40,9 @@ export const BackgroundGradientAnimation = ({
   const [tgX, setTgX] = useState(0);
   const [tgY, setTgY] = useState(0);
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      document.body.style.setProperty(
-      "--gradient-background-start",
-      gradientBackgroundStart
-    );
-    document.body.style.setProperty(
-      "--gradient-background-end",
-      gradientBackgroundEnd
-    );
+  if (typeof window !== "undefined" && typeof document !== "undefined") {
+    document.body.style.setProperty("--gradient-background-start", gradientBackgroundStart);
+    document.body.style.setProperty("--gradient-background-end", gradientBackgroundEnd);
     document.body.style.setProperty("--first-color", firstColor);
     document.body.style.setProperty("--second-color", secondColor);
     document.body.style.setProperty("--third-color", thirdColor);
@@ -57,8 +51,9 @@ export const BackgroundGradientAnimation = ({
     document.body.style.setProperty("--pointer-color", pointerColor);
     document.body.style.setProperty("--size", size);
     document.body.style.setProperty("--blending-value", blendingValue);
-    }
-  }, []);
+  }
+}, [gradientBackgroundStart, gradientBackgroundEnd, firstColor, secondColor, thirdColor, fourthColor, fifthColor, pointerColor, size, blendingValue]);
+
 
   useEffect(() => {
     if (!interactiveRef.current) return;
